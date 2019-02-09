01ac3ea9cb86db8637af6a596d634ae2c0e645e5
roodiaz <roabigail.diaz@gmail.com>
2019-02-08 22:02:14 -0300

    ejercicio10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,16 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+    
+    var importe
+    
+        importe=document.getElementById("importe").value;
+        importe=parseInt(importe);
+    
+    
+    
+    var resultado
+        
+        resultado=parseInt(importe)+parseInt(importe)*0.25;
+        document.getElementById("resultado").value=resultado;
 }
