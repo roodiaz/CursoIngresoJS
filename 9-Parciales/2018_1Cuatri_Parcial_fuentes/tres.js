51114794513f164d0fcc532794ada85778decae3
unknown <roabigail.diaz@gmail.com>
2019-02-09 01:12:19 -0300

    parcial 3

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,17 @@
 function mostrar()
 {
 
+    var precio
+    var porcentaje
+    var final
+
+    precio=prompt("Ingresar precio");
+    porcentaje=prompt("Ingresar porcentaje");
+
+    precio=parseInt(precio);
+    porcentaje=parseInt(porcentaje)/100;
+
+
+    final=parseInt(precio * porcentaje);
+    document.getElementById("elPrecioFinal").value=final;
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -2,4 +2,17 @@
 function mostrar()
 {
 
+    var ancho
+    var largo
+    var perimetro1
+
+    ancho=prompt("Ingresar ancho");
+    largo=prompt("Ingresar largo");
+
+    ancho=parseInt(ancho);
+    largo=parseInt(largo);
+
+    perimetro1=parseInt(ancho + largo)*2;
+    alert(perimetro1);
+
 }
