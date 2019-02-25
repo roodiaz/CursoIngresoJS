de377dc00c8ba29a07c12476f70fcd437c3e7215
unknown <roabigail.diaz@gmail.com>
2019-02-24 22:29:38 -0300

    while7

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,16 +1,25 @@
 function mostrar()
 {
 
-	var contador=0;
-	var acumulador=0;
-  var respuesta=confirm()
+  var acumulador=0;
+  var contador=0;
+  var respuesta = confirm("Desea agregar numeros?")
+  var numeros;
 
   while(respuesta)
   {
+    contador++
+    numeros = prompt("Ingrese numero");
+    numeros = parseInt(numeros);
+    
+    respuesta=confirm("Desea seguir agreando numeros?");
+    
+    acumulador+=numeros;
+    
+    
+  } while(isNaN(numeros));
 
-  }
-
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/contador;
+  documento . getElementById ("suma"). valor = acumulador;
+  documento . getElementById ("promedio"). valor  = acumulador / contador;
 
 }//FIN DE LA FUNCIÓN
