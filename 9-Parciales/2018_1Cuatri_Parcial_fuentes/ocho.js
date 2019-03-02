7931bde3db09e899715784e08b87551a4c992390
alumno <alumno>
2019-03-01 22:00:32 -0300

    parcial8

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,117 @@
 function mostrar()
-{
+{   
+    var cantPos = 0;
+    var canNeg = 0;
+    var cantPares = 0;
+    var cantImpares= 0;
+    var cantCeros = 0;
+    var sumaNeg = 0;
+    var sumaPos = 0;
+    var numMin;
+    var numMax;
+    var letraMin;
+    var letraMax;
+    var num;
+    var letra;
+    var bandera = true;
+    var respuesta = true;
+
+    while(respuesta)
+    {
+        //ingreso numero
+        num= prompt("Ingresar numero entre -100 y 100");
+        num= parseInt(num);
+
+        //valido que no sea una letra
+        while(isNaN(num))
+        {
+            num= prompt("No es un numero. Intentar de nuevo");
+            num= parseInt(num);
+        }
+        //valido que sea un numero entre -100 y 100
+        while(num >= -100 || num <=  100)
+        {
+            num= prompt("No esta entre el -100 y 100. Intentar de nuevo")
+            num= parseInt(num);
+        }
+        
+        //ingreso letra
+        letra= prompt("Ingresar letra");
+        letra= parseInt(letra);
+
+        //valido que sea una letra
+        while(letra != isNaN(letra))
+        {
+            letra= prompt("No es una letra. Intentar de nuevo");
+            letra= parseInt(letra);
+        }
+
+        //defino max y min
+        if(bandera)
+        {
+            numMax = num;
+            numMin = num;
+            letraMax = letra;
+            letraMin = letra;
+            bandera = false;
+        }
+
+        //a) cantidad pares
+        if (num % 2 == 0)
+		{
+			cantPares++;
+        }
+        //b)cantidad impares
+        else if(num % 2 != 0)
+        {
+            cantImpares++;
+        }
+        //c)cantidad ceros
+        else 
+        {
+            cantCeros++;
+        }
+
+        //d)promedio positivos
+        promedioPos=sumaPos/cantPos;
+
+        //e)suma negativos
+        if(num < 0)
+        {
+            sumaNeg+= num;
+            cantNeg++;
+        }
+
+        //f)num y letra max y min
+        if (num < numMin)
+		{
+			numMin = num;
+		}	
+		if (num > numMax)
+		{
+			numMax = num;
+        }
+        if (letra < letraMin)
+        {
+            letraMin = letra;
+        }
+        if(num > letraMax)
+        {
+            letraMax = letra;
+        }
+
+        //preguntar si sego ingresando datos
+        respuesta = confirm("¿Desea seguir ingrensando datos?");
+        
+    }
+
+    document.write("La cantidad de numeros pares es "+cantPares+"<br>");
+    document.write("La cantidad de numeros impares es "+cantImpares+"<br>");
+    document.write("La cantidad de ceros es "+cantCeros+"<br>");
+    document.write("El promedio de los numeros positivos es "+promedioPos+"<br>");
+    document.write("La suma de los numeros negativos es "+sumaNeg+"<br>");
+    document.write("El numero maximo es "+numMax+" y la letra maxima es "+letraMax+"<br>");
+    document.write("El numero minimo es "+numMin+" y la letra minima es "+letraMin+"<br>");
+
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,59 @@
 function mostrar()
 {
+    var nota;
+    var sexo;
+    var sumaNotas = 0;
+    var i=0;
+    var notaMasBaja = 10;
+    var sexoDeNotaMasBaja;
+    var varonesConNotaMayorASeis = 0;
 
+    while(i < 5)
+    {
+    nota = prompt("Ingrese nota obtenida entre 0 y 10");
+    nota = parseInt(nota);
+        
+        //valido que no sea una letra
+        while(isNaN(nota)){
+            nota = prompt("Ingrese una nota numerica");
+            nota = parseInt(nota);
+        }
+
+        //valido que sea un numero entre 0 y 10
+        while(nota < 0 || nota > 10){
+            nota = prompt("Ingrese una nota entre 0 y 10");
+            nota = parseInt(nota);  
+        }
+
+    sexo = prompt("Ingrese el sexo del alumno(F o M)");
+
+        //valido que sea F o M
+        while(sexo !="f" && sexo !="m"){
+            sexo = prompt("Ingrese F o M");
+        }
+        
+        //sumo notas para obtener promedio
+        sumaNotas = sumaNotas + nota;
+
+        //guardo nota mas baja
+        if(notaMasBaja > nota){
+            notaMasBaja = nota;
+            sexoDeNotaMasBaja = sexo;
+        }
+
+        //calculo cant de M con nota mayor o igual a 6
+        if(sexo == "m" && nota >= 6){
+            varonesConNotaMayorASeis++;
+        }
+
+    i++;
+    }
+
+    //alert("El promedio de las notas es: " + sumaNotas / i);
+    //alert("La nota mas baja es: " + notaMasBaja + " y su sexo es: " + sexoDeNotaMasBaja);
+    //alert("Cantidad de varones con nota mayor o igual a 6: " + varonesConNotaMayorASeis);
+
+    alert("El promedio de las notas es: " + sumaNotas / i +
+    "\nLa nota mas baja es: " + notaMasBaja + " y su sexo es: " + sexoDeNotaMasBaja +
+    "\nCantidad de varones con nota mayor o igual a 6: " + varonesConNotaMayorASeis);
 }
